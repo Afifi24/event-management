b5da73fe15b408cd762c46df85f88b1ad7af9ecf
afifi brahim <afifi71brahim@gmail.com>
2025-02-14 00:35:34 +0100

    fix user action

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -1,4 +1,4 @@
-"user server";
+"use server";
 
 import { CreateUserParams, UpdateUserParams } from "@/types";
 import { handleError } from "../utils";
@@ -6,6 +6,7 @@ import { connectToDatabase } from "../database";
 import User from "../database/models/user.model";
 import { revalidatePath } from "next/cache";
 import Order from "../database/models/order.model";
+import Event from "../database/models/event.model";
 
 export const createUser = async (user: CreateUserParams) => {
   try {
