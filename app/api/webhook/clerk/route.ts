c4d0034514b2c050cf6ab9771bc7334202afad62
afifi brahim <afifi71brahim@gmail.com>
2025-02-14 12:34:52 +0100

    fix clerkClient issue

--- app/api/webhook/clerk/route.ts
+++ app/api/webhook/clerk/route.ts
@@ -68,9 +68,9 @@ export async function POST(req: Request) {
     };
 
     const newUser = await createUser(user);
-
+    const client = await clerkClient();
     if (newUser) {
-      await clerkClient.users.updateUserMetadata(id, {
+      await client.users.updateUserMetadata(id, {
         publicMetadata: {
           userId: newUser._id,
         },
@@ -84,8 +84,8 @@ export async function POST(req: Request) {
     const { id, image_url, first_name, last_name, username } = evt.data;
 
     const user = {
-      firstName: first_name,
-      lastName: last_name,
+      firstName: first_name!,
+      lastName: last_name!,
       username: username!,
       photo: image_url,
     };
